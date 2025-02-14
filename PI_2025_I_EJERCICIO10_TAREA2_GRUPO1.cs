cdda387a3d5e47038594712f4210ec814345fe71
2003genesis <genesis.ramos@ujcv.edu.hn>
2025-02-14 16:55:32 -0600

    Add files via upload

---
+++ PI_2025_I_EJERCICIO10_TAREA2_GRUPO1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace PI_2025_I_EJERCICIO10_TAREA2_GRUPO1
+{
+    internal class PI_2025_I_EJERCICIO10_TAREA2_GRUPO1
+    {
+
+        // Función para convertir de Fahrenheit a Centígrados
+        static double  Centigrados(double fahrenheit)
+            {
+                return (fahrenheit - 32) * 5 / 9;  //formula para convertir
+            }
+
+            // Función para convertir  de Centígrados a Fahrenheit
+            static double Fahrenheit(double centigrados)
+            {
+                return (centigrados * 9 / 5) + 32; //Formula para convertir
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conversión de temperaturas:");
+
+            // Imprimir tabla de Fahrenheit a Centígrados
+            Console.WriteLine("\nTemperaturas en grados Fahrenheit y sus equivalentes en grados Centígrados:");
+            Console.WriteLine("Fahrenheit | Centígrados");
+            for (int i = 32; i <= 212; i += 20)  // 32 hasta 212 de 20 en 20 grados
+            {
+                double celsius = Centigrados(i);
+                Console.WriteLine($"{i,10} | {celsius,12:F2}");
+            }
+
+            // Separador
+            Console.WriteLine("\n--------------------------------------------");
+
+            // Aqui para imprimir tabla
+            Console.WriteLine("\nTemperaturas en grados Centígrados y sus equivalentes en grados Fahrenheit:");
+            Console.WriteLine("Centígrados | Fahrenheit");
+            for (int i = 0; i <= 100; i += 10)  // 0 hasta 100 de 10 en 10 grados
+            {
+                double fahrenheit = Fahrenheit(i);
+                Console.WriteLine($"{i,12} | {fahrenheit,10:F2}");
+            }
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
---
+++ PI_2025_I_TAREA2_GRUPO1_EJERCICIO9.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_2025_I_TAREA2_GRUPO1_EJERCICIO9
+{
+    internal class PI_2025_I_TAREA2_GRUPO1_EJERCICIO9
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa su salario:");
+            double salario = Convert.ToDouble(Console.ReadLine());
+            double aumento = 0;
+
+            if (salario >= 0 && salario <= 4999)
+            {
+                aumento = salario * 0.20;  // Su sueldo aumentara un 20%
+            }
+            else if (salario >= 5000 && salario <= 9999)
+            {
+                aumento = salario * 0.10;  // Su sueldo aumentara un 10%
+            }
+            else if (salario >= 10000 && salario <= 14999)
+            {
+                aumento = salario * 0.05;  // Su sueldo aumentara un 5%
+            }
+            else if (salario >= 15000)
+            {
+                aumento = salario * 0.03;  // Su sueldo aumentara un 3%
+            }
+            else
+            {
+                Console.WriteLine("El salario ingresado no es válido.");
+                return;
+            }
+
+            //Calculando el sueldo final
+
+            double salarioFinal = salario + aumento;
+            Console.WriteLine("El aumento es: " + aumento);
+            Console.WriteLine("El salario final después del aumento es: " + salarioFinal);
+
+        }
+    }
+    }
+}
