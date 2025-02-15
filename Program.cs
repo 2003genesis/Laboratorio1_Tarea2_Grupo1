2658c1cbb531e1c2d177758067b216f647dc14cd
claudiasierra18 <claudiasierra1803@gmail.com>
2025-02-15 23:41:23 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,47 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PI_2025_I_EJERCICIO9_TAREA2_GRUPO1
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Ingresa su salario:");
-            double salario = Convert.ToDouble(Console.ReadLine());
-            double aumento = 0;
+        Console.Write("Ingrese el número de horas estacionado: ");
+        int horas = int.Parse(Console.ReadLine());
 
-            if (salario >= 0 && salario <= 4999)
-            {
-                aumento = salario * 0.20;  // Su sueldo aumentara un 20%
-            }
-            else if (salario >= 5000 && salario <= 9999)
-            {
-                aumento = salario * 0.10;  // Su sueldo aumentara un 10%
-            }
-            else if (salario >= 10000 && salario <= 14999)
-            {
-                aumento = salario * 0.05;  // Su sueldo aumentara un 5%
-            }
-            else if (salario >= 15000)
-            {
-                aumento = salario * 0.03;  // Su sueldo aumentara un 3%
-            }
-            else
-            {
-                Console.WriteLine("El salario ingresado no es válido.");
-                return;
-            }
+        double total = CalcularCargos(horas);
+        Console.WriteLine($"El total a pagar es: ${total:F2}");
+    }
 
-            //Calculando el sueldo final
-
-            double salarioFinal = salario + aumento;
-            Console.WriteLine("El aumento es: " + aumento);
-            Console.WriteLine("El salario final después del aumento es: " + salarioFinal);
-
-        }
+    static double CalcularCargos(int horas)
+    {
+        if (horas <= 3)
+            return 2.00;
+        else if (horas < 24)
+            return 2.00 + (horas - 3) * 0.50;
+        else
+            return 10.00;
     }
 }
+
