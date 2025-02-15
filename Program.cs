4e6ff7062b715a0542e8fb3ae8e968aa7085a505
claudiasierra18 <claudiasierra1803@gmail.com>
2025-02-15 23:44:06 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,21 +4,18 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Ingrese el número de horas estacionado: ");
-        int horas = int.Parse(Console.ReadLine());
+        Console.Write("Ingrese un número: ");
+        int numero = int.Parse(Console.ReadLine());
 
-        double total = CalcularCargos(horas);
-        Console.WriteLine($"El total a pagar es: ${total:F2}");
-    }
-
-    static double CalcularCargos(int horas)
-    {
-        if (horas <= 3)
-            return 2.00;
-        else if (horas < 24)
-            return 2.00 + (horas - 3) * 0.50;
+        if (numero >= 0 && numero < 10)
+            Console.WriteLine("1");
+        else if (numero >= 10 && numero < 100)
+            Console.WriteLine("2");
+        else if (numero >= 100 && numero < 1000)
+            Console.WriteLine("3");
+        else if (numero >= 1000 && numero < 10000)
+            Console.WriteLine("4");
         else
-            return 10.00;
+            Console.WriteLine("Mayor a 5");
     }
 }
-
