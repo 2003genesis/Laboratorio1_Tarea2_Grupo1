17e507644991d178e95f56fd22e0b087fcc9edcf
claudiasierra18 <claudiasierra1803@gmail.com>
2025-02-15 23:50:14 +0100

    Add files via upload

---
+++ Ejercicio5 Contar Cifras.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Ejercicio5 Contar Cifras
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero >= 0 && numero < 10)
+            Console.WriteLine("1");
+        else if (numero >= 10 && numero < 100)
+            Console.WriteLine("2");
+        else if (numero >= 100 && numero < 1000)
+            Console.WriteLine("3");
+        else if (numero >= 1000 && numero < 10000)
+            Console.WriteLine("4");
+        else
+            Console.WriteLine("Mayor a 5");
+    }
+}
